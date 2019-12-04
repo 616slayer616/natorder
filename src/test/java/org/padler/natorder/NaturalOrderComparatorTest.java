1d8b23dd820f0e281d73b0097732261695f9bea6
Patrick Adler <patrick.adler@cegeka.de>
2019-12-04 09:46:16 +0100

    Replace hamcrest with assertJ

--- src/test/java/org/padler/natorder/NaturalOrderComparatorTest.java
+++ src/test/java/org/padler/natorder/NaturalOrderComparatorTest.java
@@ -7,8 +7,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class NaturalOrderComparatorTest {
@@ -19,72 +18,72 @@ class NaturalOrderComparatorTest {
             "pic2", "pic02", "pic02a", "pic3", "pic4", "pic 4 else", "pic 5", "pic05",
             "pic 5 something", "pic 6", "pic   7", "pic100", "pic100a", "pic120", "pic121",
             "pic02000", "tom", "x2-g8", "x2-y7", "x2-y08", "x8-y8");
-    private List<String> input = Arrays.asList("1-2", "1-02", "1-20", "10-20", "fred", "jane", "pic01",
+    private List<String> unsorted = Arrays.asList("1-2", "1-02", "1-20", "10-20", "fred", "jane", "pic01",
             "pic2", "pic02", "pic02a", "pic3", "pic4", "pic 4 else", "pic 5", "pic05",
             "pic 5 something", "pic 6", "pic   7", "pic100", "pic100a", "pic120", "pic121",
             "pic02000", "tom", "x2-g8", "x2-y7", "x2-y08", "x8-y8");
 
     @Test
     void simple() {
-        List<String> unSorted = Arrays.asList("2", "3", "1");
+        List<String> unsorted = Arrays.asList("2", "3", "1");
         List<String> sorted = Arrays.asList("1", "2", "3");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void simpleWithChars() {
-        List<String> unSorted = Arrays.asList("2", "3", "1B", "1A", "1");
+        List<String> unsorted = Arrays.asList("2", "3", "1B", "1A", "1");
         List<String> sorted = Arrays.asList("1", "1A", "1B", "2", "3");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void sortHouseNumbers() {
-        List<String> input = Arrays.asList("A", "1007A", "1A", "1B", "2A", "8052A", "2165A", "4472A", "2500A", "4604A");
+        List<String> unsorted = Arrays.asList("A", "1007A", "1A", "1B", "2A", "8052A", "2165A", "4472A", "2500A", "4604A");
         List<String> sorted = Arrays.asList("1A", "1B", "2A", "1007A", "2165A", "2500A", "4472A", "4604A", "8052A", "A");
-        input.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(input, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void defaultTest() {
-        input.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(input, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void scramble42() {
-        Collections.shuffle(input, new Random(42));
+        Collections.shuffle(unsorted, new Random(42));
 
-        input.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(input, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void scramble420() {
-        Collections.shuffle(input, new Random(420));
+        Collections.shuffle(unsorted, new Random(420));
 
-        input.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(input, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void scramble1337() {
-        Collections.shuffle(input, new Random(1337));
+        Collections.shuffle(unsorted, new Random(1337));
 
-        input.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(input, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
@@ -116,72 +115,72 @@ class NaturalOrderComparatorTest {
 
     @Test
     void floats() {
-        List<String> unSorted = Arrays.asList("0.9", "1.0c", "1.2", "1.3", "0.6", "1.1", "0.7", "0.3", "1.0b", "1.0", "0.8");
+        List<String> unsorted = Arrays.asList("0.9", "1.0c", "1.2", "1.3", "0.6", "1.1", "0.7", "0.3", "1.0b", "1.0", "0.8");
         List<String> sorted = Arrays.asList("0.3", "0.6", "0.7", "0.8", "0.9", "1.0", "1.0b", "1.0c", "1.1", "1.2", "1.3");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void floatsWithCommas() {
-        List<String> unSorted = Arrays.asList("0,9", "1,0c", "1,2", "1,3", "0,6", "1,1", "0,7", "0,3", "1,0b", "1,0", "0,8");
+        List<String> unsorted = Arrays.asList("0,9", "1,0c", "1,2", "1,3", "0,6", "1,1", "0,7", "0,3", "1,0b", "1,0", "0,8");
         List<String> sorted = Arrays.asList("0,3", "0,6", "0,7", "0,8", "0,9", "1,0", "1,0b", "1,0c", "1,1", "1,2", "1,3");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void wikipediaTest() {
-        List<String> unSorted = Arrays.asList("z11", "z2");
+        List<String> unsorted = Arrays.asList("z11", "z2");
         List<String> sorted = Arrays.asList("z2", "z11");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void wikipediaTestStable() {
-        List<String> unSorted = Arrays.asList("z2", "z11");
+        List<String> unsorted = Arrays.asList("z2", "z11");
         List<String> sorted = Arrays.asList("z2", "z11");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void files() {
-        List<String> unSorted = Arrays.asList("file1.txt", "file2.txt", "file10.txt");
+        List<String> unsorted = Arrays.asList("file1.txt", "file2.txt", "file10.txt");
         List<String> sorted = Arrays.asList("file1.txt", "file2.txt", "file10.txt");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void versions() {
-        List<String> unSorted = Arrays.asList("1.2.9.1", "1.2.10.5");
+        List<String> unsorted = Arrays.asList("1.2.9.1", "1.2.10.5");
         List<String> sorted = Arrays.asList("1.2.9.1", "1.2.10.5");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void versionsStable() {
-        List<String> unSorted = Arrays.asList("1.2.10.5", "1.2.9.1");
+        List<String> unsorted = Arrays.asList("1.2.10.5", "1.2.9.1");
         List<String> sorted = Arrays.asList("1.2.9.1", "1.2.10.5");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
 }
--- src/test/java/org/padler/natorder/RosettacodeTest.java
+++ src/test/java/org/padler/natorder/RosettacodeTest.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 /**
  * These tests are inspired by https://rosettacode.org/wiki/Natural_sorting
@@ -18,115 +18,115 @@ class RosettacodeTest {
 
     @Test
     void ignoringLeadingSpaces() {
-        List<String> unSorted = Arrays.asList("ignore leading spaces: 2-2", " ignore leading spaces: 2-1", "  ignore leading spaces: 2+0", "   ignore leading spaces: 2+1");
+        List<String> unsorted = Arrays.asList("ignore leading spaces: 2-2", " ignore leading spaces: 2-1", "  ignore leading spaces: 2+0", "   ignore leading spaces: 2+1");
         List<String> sorted = Arrays.asList("  ignore leading spaces: 2+0", "   ignore leading spaces: 2+1", " ignore leading spaces: 2-1", "ignore leading spaces: 2-2");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void ignoringMultipleAdjacentSpaces() {
-        List<String> unSorted = Arrays.asList("ignore m.a.s spaces: 2-2", "ignore m.a.s  spaces: 2-1", "ignore m.a.s   spaces: 2+0", "ignore m.a.s    spaces: 2+1");
+        List<String> unsorted = Arrays.asList("ignore m.a.s spaces: 2-2", "ignore m.a.s  spaces: 2-1", "ignore m.a.s   spaces: 2+0", "ignore m.a.s    spaces: 2+1");
         List<String> sorted = Arrays.asList("ignore m.a.s   spaces: 2+0", "ignore m.a.s    spaces: 2+1", "ignore m.a.s  spaces: 2-1", "ignore m.a.s spaces: 2-2");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     @Disabled("The result is different from that on rosettacode")
     void equivalentWhitespaceCharacters() {
-        List<String> unSorted = Arrays.asList("Equiv. spaces: 3-3", "Equiv.\rspaces: 3-2", "Equiv.\014spaces: 3-1", "Equiv.\013spaces: 3+0", "Equiv.\nspaces: 3+1", "Equiv.\tspaces: 3+2");
+        List<String> unsorted = Arrays.asList("Equiv. spaces: 3-3", "Equiv.\rspaces: 3-2", "Equiv.\014spaces: 3-1", "Equiv.\013spaces: 3+0", "Equiv.\nspaces: 3+1", "Equiv.\tspaces: 3+2");
         List<String> sorted = Arrays.asList("Equiv.\013spaces: 3+0", "Equiv.\nspaces: 3+1", "Equiv.\014spaces: 3-1", "Equiv.\tspaces: 3+2", "Equiv.\rspaces: 3-2", "Equiv. spaces: 3-3");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void equivalentWhitespaceCharacters_noTab() {
-        List<String> unSorted = Arrays.asList("Equiv. spaces: 3-3", "Equiv.\rspaces: 3-2", "Equiv.\nspaces: 3+1");
+        List<String> unsorted = Arrays.asList("Equiv. spaces: 3-3", "Equiv.\rspaces: 3-2", "Equiv.\nspaces: 3+1");
         List<String> sorted = Arrays.asList("Equiv.\nspaces: 3+1", "Equiv.\rspaces: 3-2", "Equiv. spaces: 3-3");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void caseIndependent() {
-        List<String> unSorted = Arrays.asList("cASE INDEPENENT: 3-2", "caSE INDEPENENT: 3-1", "casE INDEPENENT: 3+0", "case INDEPENENT: 3+1");
+        List<String> unsorted = Arrays.asList("cASE INDEPENENT: 3-2", "caSE INDEPENENT: 3-1", "casE INDEPENENT: 3+0", "case INDEPENENT: 3+1");
         List<String> sorted = Arrays.asList("casE INDEPENENT: 3+0", "case INDEPENENT: 3+1", "caSE INDEPENENT: 3-1", "cASE INDEPENENT: 3-2");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void numericFieldsAsNumerics() {
-        List<String> unSorted = Arrays.asList("foo100bar99baz0.txt", "foo100bar10baz0.txt", "foo1000bar99baz10.txt", "foo1000bar99baz9.txt");
+        List<String> unsorted = Arrays.asList("foo100bar99baz0.txt", "foo100bar10baz0.txt", "foo1000bar99baz10.txt", "foo1000bar99baz9.txt");
         List<String> sorted = Arrays.asList("foo100bar10baz0.txt", "foo100bar99baz0.txt", "foo1000bar99baz9.txt", "foo1000bar99baz10.txt");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void titleSorts() {
-        List<String> unSorted = Arrays.asList("The Wind in the Willows", "The 40th step more", "The 39 steps", "Wanda");
+        List<String> unsorted = Arrays.asList("The Wind in the Willows", "The 40th step more", "The 39 steps", "Wanda");
         List<String> sorted = Arrays.asList("The 39 steps", "The 40th step more", "The Wind in the Willows", "Wanda");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void equivalentAccentedCharactersAndCase() {
-        List<String> unSorted = Arrays.asList("Equiv. ý accents: 2-2", "Equiv. Ý accents: 2-1", "Equiv. y accents: 2+0", "Equiv. Y accents: 2+1");
+        List<String> unsorted = Arrays.asList("Equiv. ý accents: 2-2", "Equiv. Ý accents: 2-1", "Equiv. y accents: 2+0", "Equiv. Y accents: 2+1");
         List<String> sorted = Arrays.asList("Equiv. y accents: 2+0", "Equiv. Y accents: 2+1", "Equiv. Ý accents: 2-1", "Equiv. ý accents: 2-2");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     @Disabled("The result is different from that on rosettacode")
     void separatedLigatures() {
-        List<String> unSorted = Arrays.asList("\u0132 ligatured ij", "no ligature");
+        List<String> unsorted = Arrays.asList("\u0132 ligatured ij", "no ligature");
         List<String> sorted = Arrays.asList("\u0132 ligatured ij", "no ligature");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     @Disabled("The result is different from that on rosettacode")
     void characterReplacements() {
-        List<String> unSorted = Arrays.asList("Start with an \u0292: 2-2", "Start with an \u017f: 2-1", "Start with an ß: 2+0", "Start with an s: 2+1");
+        List<String> unsorted = Arrays.asList("Start with an \u0292: 2-2", "Start with an \u017f: 2-1", "Start with an ß: 2+0", "Start with an s: 2+1");
         List<String> sorted = Arrays.asList("Start with an s: 2+1", "Start with an ſ: 2-1", "Start with an ʒ: 2-2", " Start with an ß: 2 + 0");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
     @Test
     void characterReplacements_() {
-        List<String> unSorted = Arrays.asList("Start with an \u0292: 2-2", "Start with an \u017f: 2-1", "Start with an ß: 2+0", "Start with an s: 2+1");
+        List<String> unsorted = Arrays.asList("Start with an \u0292: 2-2", "Start with an \u017f: 2-1", "Start with an ß: 2+0", "Start with an s: 2+1");
         List<String> sorted = Arrays.asList("Start with an s: 2+1", "Start with an ſ: 2-1", "Start with an ß: 2+0", "Start with an ʒ: 2-2");
 
-        unSorted.sort(naturalOrderComparator);
+        unsorted.sort(naturalOrderComparator);
 
-        assertThat(unSorted, is(sorted));
+        assertThat(unsorted).isEqualTo(sorted);
     }
 
 }
